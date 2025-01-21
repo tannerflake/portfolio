5b873e8df45317546d077d6a9839723ac01244e2
Tanner Flake <tannerflake@gmail.com>
2025-01-21 09:16:22 -0700

    Tweaked a few things and added a dist folder to fix issues with Netlify deployment.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this is set correctly
-  server: {
-    port: 3000,
-    open: true,
-  },
+  base: './', // Ensures assets are served correctly
 });
