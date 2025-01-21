c4599f7f622a3efa07af29e8f16e6d125c94db73
Tanner Flake <tannerflake@gmail.com>
2025-01-21 10:01:36 -0700

    Figured out why my photo wasn't loading, and added an import to fix it

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
+import headshot from '../assets/Headshot-casual.jpg';
+
 export default function About() {
   return (
     <div className="main">
       <h1>About Me</h1>
       <p>
-      Tanner Flake is a driven senior product manager with a passion for creating innovative, user-focused solutions in tech. With a Strategic Management degree from BYU’s Marriott School of Business and a stellar academic record, Tanner has quickly advanced in his career, becoming a Senior Product Manager at Arborgold in less than three years after undergrad. Based in Utah, Tanner is also a budding full-stack developer. Beyond work, Tanner is an avid reader, consuming an average of books 45 books annually for the last 5 years. He's deeply motivated by a vision of founding his own tech startup one day. He combines a sharp strategic mindset with a commitment to personal and professional growth, aiming to create meaningful impact in the tech world.
+        Tanner Flake is a driven senior product manager with a passion for creating innovative, user-focused solutions in tech. With a Strategic Management degree from BYU’s Marriott School of Business and a stellar academic record, Tanner has quickly advanced in his career, becoming a Senior Product Manager at Arborgold in less than three years after undergrad. Based in Utah, Tanner is also a budding full-stack developer. Beyond work, Tanner is an avid reader, consuming an average of books 45 books annually for the last 5 years. He's deeply motivated by a vision of founding his own tech startup one day. He combines a sharp strategic mindset with a commitment to personal and professional growth, aiming to create meaningful impact in the tech world.
       </p>
-      <img src="./src/assets/Headshot-casual.jpg" alt="Tanner Flake" className="headshot" />
+      <img src={headshot} alt="Tanner Flake" className="headshot" />
     </div>
   );
 }
